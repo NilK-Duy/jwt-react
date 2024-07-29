b824cf7cc01a64de2abaca8f8f3c77de2dcce2a8
{NilK-Albert} <{khanhduyxtkd@gmail.com}>
2024-07-29 17:29:20 +0700

    add login interface

--- src/utils/api.js
+++ src/utils/api.js
@@ -7,7 +7,15 @@ const createUserApi = (name, email, password) => {
   }
   return axios.post(URL_API, data)
 }
+const loginAPI = (email, password) => {
+  const URL_API = '/v1/api/login'
+  const data = {
+    email, password
+  }
+  return axios.post(URL_API, data)
+}
 
 export {
-  createUserApi
+  createUserApi,
+  loginAPI
 }
